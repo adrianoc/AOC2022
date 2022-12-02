e24849d7eda42f329b855c02863e22fe9c504607
Adriano Verona <adriano.verona@gmail.com>
2022-12-02 15:57:46 -0500

    day 2, part 2

--- src/day02/Program.cs
+++ src/day02/Program.cs
@@ -1,18 +1,4 @@
-﻿/*
- *                 +-------------------------+
- *                 | Rock | Paper | Scissors |
- *                 +-------------------------+
- *                 |   A  |   B   |    C     |
- * +-----------------------------------------+
- * |  Rock     | X |   4  |   1   |    6     |
- * +-----------------------------------------+
- * |  Paper    | Y |   8  |   5   |    2     |
- * +-----------------------------------------+
- * |  Scissors | Z |   3  |   9   |    6     |
- * +-----------------------------------------+
- */
-
-const byte RockPoints = 1;
+﻿const byte RockPoints = 1;
 const byte PaperPoints = 2;
 const byte ScissorsPoints = 3;
 
@@ -20,41 +6,26 @@ const byte WinPoints = 6;
 const byte DrawPoints = 3;
 const byte LosePoints = 0;
 
-var RockScissorsWin      = new PointsPerResult("CX".GetHashCode(), WinPoints + RockPoints);
-var RockRockDraw         = new PointsPerResult("AX".GetHashCode(), DrawPoints + RockPoints);
-var RockPaperLose        = new PointsPerResult("BX".GetHashCode(), LosePoints + RockPoints);
-
-var PaperRockWin         = new PointsPerResult("AY".GetHashCode(), WinPoints + PaperPoints);
-var PaperPaperDraw       = new PointsPerResult("BY".GetHashCode(), DrawPoints + PaperPoints);
-var PaperScissorsLose    = new PointsPerResult("CY".GetHashCode(), LosePoints + PaperPoints);
-
-var ScissorsPaperWin     = new PointsPerResult("BZ".GetHashCode(), WinPoints + ScissorsPoints);
-var ScissorsScissorsDraw = new PointsPerResult("CZ".GetHashCode(), DrawPoints + ScissorsPoints);
-var ScissorsRockLose     = new PointsPerResult("AZ".GetHashCode(), LosePoints + ScissorsPoints);
-
-
-var pointsPerRoundResult = new []
-{
-    RockScissorsWin,
-    RockRockDraw,
-    RockPaperLose,
-
-    PaperRockWin,
-    PaperPaperDraw,
-    PaperScissorsLose,
-
-    ScissorsPaperWin,
-    ScissorsScissorsDraw,
-    ScissorsRockLose,
-};
-
 if (args.Length == 1 && args[0] == "test")
 {
-    Test(new [] { "A Y", "B X", "C Z" }, 15);
-    Test(new [] { "A X", "B Y", "C Z" }, DrawPoints * 3 + RockPoints + PaperPoints + ScissorsPoints);
-    Test(new [] { "B X", "C Y", "A Z" }, LosePoints * 3 + RockPoints + PaperPoints + ScissorsPoints);
-    Test(new [] { "C X", "A Y", "B Z" }, WinPoints * 3  + RockPoints + PaperPoints + ScissorsPoints);
-    Test(new [] { "C X", "B Y", "A Z" }, WinPoints + LosePoints + DrawPoints + RockPoints + PaperPoints + ScissorsPoints);
+    Test(
+        new [] { "A Y", "B X", "C Z" }, 
+        DrawPoints + RockPoints + 
+        LosePoints + RockPoints +
+        WinPoints + RockPoints);
+
+    Test(
+        new [] { "A X", "B Y", "C Z" },
+        LosePoints + ScissorsPoints +
+        DrawPoints + PaperPoints +
+        WinPoints + RockPoints);
+
+    Test(
+        new [] { "B X", "C Y", "A Z" }, 
+        LosePoints + RockPoints +
+        DrawPoints + ScissorsPoints +
+        WinPoints + PaperPoints);
+
     return;
 }
 
@@ -62,20 +33,33 @@ var input = System.IO.File.ReadAllLines("../../inputs/day02/input");
 var result = ComputePoints(input);
 System.Console.WriteLine($"Total: {result}");
 
-uint ComputePoints(string []input) 
+int ComputePoints(string []input) 
 {
-    uint points = 0;
+    const char Lose = 'X';
+    const char Draw = 'Y';
+    const char Win = 'Z';
+
+    var rules = new []
+    {
+        //       Z-X             Z-Y             Z-X
+        //       Win,            Draw,           Lose               // Other
+        new [] { PaperPoints,    RockPoints,     ScissorsPoints },  // Rock     (A): 0 -> A - A
+        new [] { ScissorsPoints, PaperPoints,    RockPoints},       // Paper    (B): 1 -> B - A
+        new [] { RockPoints,     ScissorsPoints, PaperPoints },     // Scissors (C): 2 -> C - A
+    };
+
+    int points = 0;
     for(int i = 0; i < input.Length; i++)
     {
-        var roundResult = input[i].Replace(" ", string.Empty).GetHashCode();
-        for(byte j = 0; j < pointsPerRoundResult.Length; j++)
+        var pointsFromRound = input[i][2] switch
         {
-            if (roundResult == pointsPerRoundResult[j].Hash)
-            {
-                points += pointsPerRoundResult[j].Points;
-                break;
-            }
-        }
+            Win =>  WinPoints  + rules[input[i][0] - 'A'][0],
+            Draw => DrawPoints + rules[input[i][0] - 'A'][1],
+            Lose => LosePoints + rules[input[i][0] - 'A'][2],
+            _ => throw new InvalidOperationException("Unexpected opponent round: {input[i][0]}")
+        };
+
+        points += pointsFromRound;
     }
     
     return points;

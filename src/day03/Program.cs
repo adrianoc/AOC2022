195fe242282d3d3a1f17e62d7d2ad37473b96948
Adriano Verona <adriano.verona@gmail.com>
2022-12-03 01:15:22 -0500

    day 3, part 2

--- src/day03/Program.cs
+++ src/day03/Program.cs
@@ -14,41 +14,34 @@ int ComputePrioritySum(string []rucksacks)
 {
     int prioritySum = 0;
 
-    Parallel.ForEach(
-        rucksacks,
-        rucksack => Interlocked.Add(ref prioritySum, PriorityForRucksack(rucksack)));
+    for(int i = 0; i < rucksacks.Length; i += 3)
+    {
+        prioritySum += PriorityForGroup(rucksacks[i], rucksacks[i+1], rucksacks[i+2]);
+    }
 
     return prioritySum;
 }
 
-int PriorityForRucksack(string rucksack)
+int PriorityForGroup(string elf1Items, string elf2Items, string elf3Items)
 {
-    var rs = rucksack.AsSpan();
-    if (rs.Length % 2 != 0)
+    for(int i = 0; i < elf1Items.Length; i++)
     {
-        throw new ArgumentException($"Rucksack has an odd # of items ({rucksack.Length}).", nameof(rucksack));
-    }
-
-    var firstCompartment = rs.Slice(0, rucksack.Length / 2);
-    var secondCompartment = rs.Slice(rucksack.Length / 2);
-
-    for(int i = 0; i < firstCompartment.Length; i++)
-    {
-        for(int j = 0; j < secondCompartment.Length; j++)
+        for(int j = 0; j < elf2Items.Length; j++)
         {
-            if (firstCompartment[i] == secondCompartment[j])
+            if (elf1Items[i] == elf2Items[j])
             {
-                return Char.IsLower(firstCompartment[i]) 
-                            ? (firstCompartment[i] - 'a' +1) 
-                            : (firstCompartment[i] - 'A' + 27);
+                for(int k = 0; k < elf3Items.Length; k++)
+                {
+                    if (elf3Items[k] == elf2Items[j])
+                    {
+                        return Char.IsLower(elf1Items[i]) 
+                                    ? (elf1Items[i] - 'a' + 1) 
+                                    : (elf1Items[i] - 'A' + 27);
+                    }
+                }
             }
         }
     }
-
-#if VALIDATE_VALUES
-
-    throw new Exception("No common items found.");
-#endif
 
     return 0;
 }
@@ -66,9 +59,9 @@ void Test()
     };
 
     var computedPrioritySum = ComputePrioritySum(input);
-    if (157 != computedPrioritySum)
+    if (70 != computedPrioritySum)
     {
-        throw new Exception($"Expected sum = 157, got {computedPrioritySum}");
+        throw new Exception($"Expected sum = 70, got {computedPrioritySum}");
     }
 
     System.Console.WriteLine("Success.");
